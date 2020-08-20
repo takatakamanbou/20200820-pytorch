1c755a6d467d9b3e7c48737e098b85075f3cdcce
TAKAHASHI <takataka@tortoise6.math.ryukoku.ac.jp>
2020-08-20 16:07:53 +0900

    update LFWDataset.py

--- LFWDataset.py
+++ LFWDataset.py
@@ -9,33 +9,42 @@ class LFWDataset(torch.utils.data.Dataset):
 
     ndatL = 4000
     ndatT = 1721
-    imageShape = (96, 128, 3)
+    imageShape = (128, 96, 3)
     ndim = np.prod(imageShape)
 
 
-    def __init__(self, pathStr='./data/lfw-selected/', LT='L'):
+    def __init__(self, dataRoot='./data/lfw-selected/', LT='L'):
+
+        self.dataRoot = os.path.normpath(dataRoot)
+        assert os.path.isdir(self.dataRoot)
+        print(f'# dataRoot = {self.dataRoot}')
+
+        # reading the mean
+        fn = os.path.join(self.dataRoot, 'meanL.npy')
+        self.meanL = np.load(fn)
 
         assert LT == 'L' or LT == 'T'
         self.LT = LT
-
-        self.path = os.path.join(os.path.normpath(pathStr), LT)
-        assert os.path.isdir(self.path)
-        print(f'# {self.path}')
-
-        # reading the attribute information
-        with open(os.path.join(self.path, 'attributes.pickle'), 'rb') as f:
-            rv = pickle.load(f)
-        self.attrList = rv['list']
-        self.ndat = len(self.attrList)
+        dataLT = os.path.join(self.dataRoot, LT)
+        if LT == 'L':
+            self.ndat = LFWDataset.ndatL
+        else:
+            self.ndat = LFWDataset.ndatT
 
         # reading the images
-        fn = os.path.join(self.path, 'img0000.png')
+        fn = os.path.join(dataLT, 'img0000.png')
         img = cv2.imread(fn)
+        assert img.shape == LFWDataset.imageShape
         X = np.empty((self.ndat,) + img.shape)
         for i in range(self.ndat):
-            print(i)
-            fn = os.path.join(self.path, f'img{i:04d}.png')
+            fn = os.path.join(dataLT, f'img{i:04d}.png')
+            #print(fn)
             X[i, ::] = cv2.imread(fn)
+
+        # mean subtraction & scaling
+        X -= self.meanL
+        X /= 255.0
+
         self.X = X
 
 
@@ -52,7 +61,11 @@ class LFWDataset(torch.utils.data.Dataset):
 
 if __name__ == '__main__':
 
+    #import torchvision
+
     ds = LFWDataset(LT='T')
     print(len(ds))
     #print(ds[0])
     print(ds.ndim)
+
+    #dl = torch.utils.data.DataLoader()
