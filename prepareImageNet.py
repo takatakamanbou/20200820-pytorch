23d28bdcc87e25fc2ad87cb4d44d4cb49da23749
TAKAHASHI Takashi <takataka@tortoiseF.local>
2020-10-03 21:41:23 +0900

    ImageNet.py

--- prepareImageNet.py
+++ prepareImageNet.py
@@ -6,5 +6,5 @@ import torchvision
 #p = '/mnt/tlab-nas/share/data/ILSVRC2012'
 p = '/Users/takataka/Desktop/ILSVRC2012'
 
-#d = torchvision.datasets.ImageNet(p)
-d = torchvision.datasets.ImageNet(p, split='val')
+dL = torchvision.datasets.ImageNet(p)
+dV = torchvision.datasets.ImageNet(p, split='val')
--- testImageNet.py
+++ testImageNet.py
@@ -2,9 +2,10 @@ import numpy as numpy
 import torch
 import torchvision
 
-#p = '/Volumes/share/data/ILSVRC2012'
+p = '/Volumes/share/data/ILSVRC2012'
 #p = '/mnt/tlab-nas/share/data/ILSVRC2012'
-p = '/Users/takataka/Desktop/ILSVRC2012'
+#p = '/Users/takataka/Desktop/ILSVRC2012'
 
-#d = torchvision.datasets.ImageNet(p)
-d = torchvision.datasets.ImageNet(p, split='val')
+#dL = torchvision.datasets.ImageNet(p)
+dV = torchvision.datasets.ImageNet(p, split='val')
+print(dV)
