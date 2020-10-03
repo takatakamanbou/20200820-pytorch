0f0b8307941b22b986e6415ec8f687fb3dd24e14
TAKAHASHI Takashi <takataka@tortoiseF.local>
2020-10-03 22:14:12 +0900

    testImageNet.py

--- testImageNet.py
+++ testImageNet.py
@@ -9,3 +9,14 @@ p = '/Volumes/share/data/ILSVRC2012'
 #dL = torchvision.datasets.ImageNet(p)
 dV = torchvision.datasets.ImageNet(p, split='val')
 print(dV)
+
+print('# number of classes:', len(dV.classes))
+
+for k, classname in enumerate(dV.classes):
+    print(k, classname)
+
+print(dV.classes[0])
+print(dV.imgs[0])
+print()
+print(dV.classes[999])
+print(dV.imgs[-1])
