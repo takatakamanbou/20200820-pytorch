8a1a7aed75019fdf7140505e4b87789197d79183
TAKAHASHI <takataka@tortoise6.math.ryukoku.ac.jp>
2020-10-03 20:01:17 +0900

    path

--- testImageNet.py
+++ testImageNet.py
@@ -3,7 +3,7 @@ import torch
 import torchvision
 
 #p = '/Volumes/share/data/ILSVRC2012'
-p = '/mnt/tlab-nas/share/data/ILSVRC2012'
-#p = '/Users/takataka/Desktop/ILSVRC2012'
+#p = '/mnt/tlab-nas/share/data/ILSVRC2012'
+p = '/Users/takataka/Desktop/ILSVRC2012'
 
 d = torchvision.datasets.ImageNet(p)
