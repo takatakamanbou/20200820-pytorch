4b664a45fcea14c2c633687ecf320675d4b882fa
TAKAHASHI <takataka@tortoise6.math.ryukoku.ac.jp>
2020-10-05 12:43:59 +0900

    p

--- testImageNet.py
+++ testImageNet.py
@@ -2,11 +2,13 @@ import numpy as numpy
 import torch
 import torchvision
 
-p = '/Volumes/share/data/ILSVRC2012'
+#p = '/Volumes/share/data/ILSVRC2012'
 #p = '/mnt/tlab-nas/share/data/ILSVRC2012'
 #p = '/Users/takataka/Desktop/ILSVRC2012'
+p = '/mnt/data/ILSVRC2012'
 
-#dL = torchvision.datasets.ImageNet(p)
+dL = torchvision.datasets.ImageNet(p)
+print(dL)
 dV = torchvision.datasets.ImageNet(p, split='val')
 print(dV)
 
