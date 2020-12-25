6771cd38563ba6d5d0109ae26a3359116c3524db
takatakamanbou <takatakamanbou@gmail.com>
2020-12-25 16:28:44 +0900

    debug

--- ilsvrc2012.py
+++ ilsvrc2012.py
@@ -4,7 +4,7 @@ import torchvision.transforms as T
 # path to ILSVRC2012
 path = '/mnt/data/ILSVRC2012'
 
-normalize = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.tor224, 0.225])
+normalize = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 ##### L #####
 
