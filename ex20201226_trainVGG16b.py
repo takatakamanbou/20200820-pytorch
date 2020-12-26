fef5edafba4c49f138d5e0d5d595d32091e8fd84
takatakamanbou <takatakamanbou@gmail.com>
2020-12-26 09:04:33 +0900

    trainVGG16b

--- ex20201226_trainVGG16b.py
+++ ex20201226_trainVGG16b.py
@@ -5,78 +5,94 @@ import torch.optim as optim
 import torchvision
 import datetime
 import sys
+import os
 
 import ilsvrc2012
 
-fnParam_IN = 'data/ex20201226_trainVGG16_epoch003.pth'
-fnParam_OUT = 'data/ex20201226_trainVGG16_epoch004.pth'
 
-# loading VGG11-bn pretrained model
-vgg16 = torchvision.models.vgg16(pretrained=False)
-print(vgg16)
+if __name__ == '__main__':
 
+    if len(sys.argv) != 2:
+        print(f'usage: {sys.argv[0]} epoch')
+        exit()
 
-# loading the parameters
-with open(fnParam_IN, mode='rb') as f:
-    vgg16.load_state_dict(torch.load(f))
+    epoch_prev = int(sys.argv[1])
 
-# device
-use_gpu_if_available = True
-use_CUDA = use_gpu_if_available and torch.cuda.is_available()
-if use_CUDA:
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
-print('# using', device)
+    fnParam_prev = f'data/ex20201226_trainVGG16_epoch{epoch_prev:03d}.pth'
+    fnParam_now = f'data/ex20201226_trainVGG16_epoch{epoch_prev+1:03d}.pth'
 
-nn = vgg16.to(device)
+    if os.path.exists(fnParam_now):
+        print(f'{fnParam_now} exists!')
+        exit()
+        
+
+    # loading VGG11-bn pretrained model
+    vgg16 = torchvision.models.vgg16(pretrained=False)
+    print(vgg16)
 
 
-# dataset & dataloader
-dL = ilsvrc2012.datasetsL
-bsize = 256
-dl = torch.utils.data.DataLoader(dL, batch_size=bsize, shuffle=True, pin_memory=use_CUDA, num_workers=16)
-nbatch = len(dl)
+    # loading the parameters
+    with open(fnParam_prev, mode='rb') as f:
+        vgg16.load_state_dict(torch.load(f))
 
-# optimizer
-#optimizer = optim.SGD(nn.parameters(), lr=0.01, momentum=0.9)
-optimizer = optim.Adam(nn.parameters(), lr=0.0001, weight_decay=0.0)
+    # device
+    use_gpu_if_available = True
+    use_CUDA = use_gpu_if_available and torch.cuda.is_available()
+    if use_CUDA:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+    print('# using', device)
+
+    nn = vgg16.to(device)
 
 
-nn.train()
+    # dataset & dataloader
+    dL = ilsvrc2012.datasetsL
+    bsize = 256
+    dl = torch.utils.data.DataLoader(dL, batch_size=bsize, shuffle=True, pin_memory=use_CUDA, num_workers=16)
+    nbatch = len(dl)
 
-nb = 1024 // bsize
-ncList = np.zeros(nb, dtype=int)
-lossList = np.zeros(nb)
+    # optimizer
+    #optimizer = optim.SGD(nn.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.Adam(nn.parameters(), lr=0.0001, weight_decay=0.0)
 
-s1 = datetime.datetime.now()
 
-for ib, rv in enumerate(dl):
-    X, lab = rv[0].to(device), rv[1].to(device)
-    optimizer.zero_grad()
-    output = F.log_softmax(nn(X), dim=1)
-    loss = F.nll_loss(output, lab)
-    loss.backward()
-    optimizer.step()
+    nn.train()
 
-    lossList[ib % nb] = loss.sum().item()
-    pred = output.max(1, keepdim=True)[1]
-    ncList[ib % nb] = pred.eq(lab.view_as(pred)).sum().item()
+    nb = 1024 // bsize
+    ncList = np.zeros(nb, dtype=int)
+    lossList = np.zeros(nb)
 
-    if ib % nb == nb - 1:
-        nc = np.sum(ncList)
-        loss_mean = np.mean(lossList)/bsize
-        print(f'{ib}/{nbatch}  {loss_mean:.6f}  {nc}/{bsize*nb} = {nc/(bsize*nb)}')
-        sys.stdout.flush()
+    s1 = datetime.datetime.now()
 
-    #if ib == 100:
-    #    break
+    for ib, rv in enumerate(dl):
+        X, lab = rv[0].to(device), rv[1].to(device)
+        optimizer.zero_grad()
+        output = F.log_softmax(nn(X), dim=1)
+        loss = F.nll_loss(output, lab)
+        loss.backward()
+        optimizer.step()
 
-s2 = datetime.datetime.now()
+        lossList[ib % nb] = loss.sum().item()
+        pred = output.max(1, keepdim=True)[1]
+        ncList[ib % nb] = pred.eq(lab.view_as(pred)).sum().item()
 
-print(s2-s1)
+        if ib % nb == nb - 1:
+            nc = np.sum(ncList)
+            loss_mean = np.mean(lossList)/bsize
+            print(f'{ib}/{nbatch}  {loss_mean:.6f}  {nc}/{bsize*nb} = {nc/(bsize*nb)}')
+            sys.stdout.flush()
 
-with open(fnParam_OUT, mode='wb') as f:
-    torch.save(nn.state_dict(), f)    
+        #if ib == 100:
+        #    break
+
+    s2 = datetime.datetime.now()
+
+    print(s2-s1)
+
+    print(f'# writing to {fnParam_now}')
+    with open(fnParam_now, mode='wb') as f:
+        torch.save(nn.state_dict(), f)    
 
 
