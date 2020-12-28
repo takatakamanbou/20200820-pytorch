2e3379a5163d1619ebd59f4df089ad468af618b0
takatakamanbou <takatakamanbou@gmail.com>
2020-12-28 14:20:43 +0900

    DCTnet3

--- DCTnet.py
+++ DCTnet.py
@@ -143,3 +143,59 @@ class DCTnet2(nn.Module):
         return X
 
 
+class DCTnet3(nn.Module):
+
+    def __init__(self):
+
+        super(DCTnet3, self).__init__()
+
+        self.conv01 = nn.Conv2d(3, 192, 8, stride = 4, bias = False)
+        W = kernelDCT2d2()
+        self.conv01.weight = nn.Parameter(torch.Tensor(W), requires_grad=False)
+        # (28, 28, 192)
+
+        self.conv02a = nn.Conv2d(192, 256, 3)
+        self.conv02b = nn.Conv2d(256, 256, 3)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        # (14, 14, 256)
+
+        self.conv03a = nn.Conv2d(256, 512, 3)
+        self.conv03b = nn.Conv2d(512, 512, 3)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        # (7, 7, 512)
+
+        self.avepool1 = nn.AdaptiveAvgPool2d((7, 7))
+
+        self.fc1 = nn.Linear(512 * 7 * 7, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 1000)
+
+        self.dropout1 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.5)
+
+    
+    def forward(self, X):
+
+        X = self.conv01(X)
+
+        X = F.relu(self.conv02a(X))
+        X = F.relu(self.conv02b(X))
+        X = self.pool1(X)
+
+        X = F.relu(self.conv03a(X))
+        X = F.relu(self.conv03b(X))
+        X = self.pool2(X)
+
+        X = self.avepool1(X)
+
+        X = X.view(-1, 512*7*7)
+
+        X = F.relu(self.fc1(X))
+        X = self.dropout1(X)
+        X = F.relu(self.fc2(X))
+        X = self.dropout2(X)
+        X = self.fc3(X)
+
+        return X
+
+
---
+++ ex20201228c_trainDCT3.py
@@ -0,0 +1,109 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import datetime
+import sys
+import os
+
+import ilsvrc2012
+import DCTnet
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2 or 3 < len(sys.argv):
+        print(f'usage: {sys.argv[0]} epoch_now [epoch_end]')
+        exit()
+
+    epoch_start = int(sys.argv[1])
+    if len(sys.argv) == 3:
+        epoch_end = int(sys.argv[2])
+    else:
+        epoch_end = epoch_start
+    epoch_prev = epoch_start - 1
+    assert epoch_prev >= 0
+
+
+    # device
+    use_gpu_if_available = True
+    use_CUDA = use_gpu_if_available and torch.cuda.is_available()
+    if use_CUDA:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+    print('# using', device)
+
+    # loading VGG11-bn pretrained model
+    #vgg16 = torchvision.models.vgg16(pretrained=False)
+    vgg16 = DCTnet.DCTnet3()
+    print(vgg16)
+
+    # loading the parameters
+    fnParam_prev = f'data/ex20201228c_trainDCT3_epoch{epoch_prev:03d}.pth'
+    if epoch_prev != 0:
+        with open(fnParam_prev, mode='rb') as f:
+            vgg16.load_state_dict(torch.load(f))
+
+    nn = vgg16.to(device)
+    nn.train()
+
+    # dataset & dataloader
+    dL = ilsvrc2012.datasets('L')
+    bsize = 256
+    dl = torch.utils.data.DataLoader(dL, batch_size=bsize, shuffle=True, pin_memory=use_CUDA, num_workers=16)
+    nbatch = len(dl)
+
+    # optimizer
+    #optimizer = optim.SGD(nn.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.Adam(nn.parameters(), lr=0.0001, weight_decay=0.0)
+
+    nb = 1024 // bsize
+    ncList = np.zeros(nb, dtype=int)
+    lossList = np.zeros(nb)
+
+    for epoch_now in range(epoch_start, epoch_end+1):
+
+        print()
+        print(f'##### epoch {epoch_now} #####')
+        print()
+        sys.stdout.flush()
+
+        fnParam_now = f'data/ex20201228c_trainDCT3_epoch{epoch_now:03d}.pth'
+        if os.path.exists(fnParam_now):
+            print(f'{fnParam_now} exists!')
+            sys.stdout.flush()
+            exit()
+
+        s1 = datetime.datetime.now()
+
+        for ib, rv in enumerate(dl):
+            X, lab = rv[0].to(device), rv[1].to(device)
+            optimizer.zero_grad()
+            output = F.log_softmax(nn(X), dim=1)
+            loss = F.nll_loss(output, lab)
+            loss.backward()
+            optimizer.step()
+
+            lossList[ib % nb] = loss.sum().item()
+            pred = output.max(1, keepdim=True)[1]
+            ncList[ib % nb] = pred.eq(lab.view_as(pred)).sum().item()
+
+            if ib % nb == nb - 1:
+                nc = np.sum(ncList)
+                loss_mean = np.mean(lossList)/bsize
+                print(f'{ib}/{nbatch}  {loss_mean:.6f}  {nc}/{bsize*nb} = {nc/(bsize*nb)}')
+                sys.stdout.flush()
+
+        s2 = datetime.datetime.now()
+
+        print(s2-s1)
+        sys.stdout.flush()
+
+        print(f'# writing to {fnParam_now}')
+        sys.stdout.flush()
+        with open(fnParam_now, mode='wb') as f:
+            torch.save(nn.state_dict(), f)    
+
+
