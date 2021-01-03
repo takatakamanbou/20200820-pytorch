d7f3927c7302dde4895526e4e425d5411d5633f8
takatakamanbou <takatakamanbou@gmail.com>
2021-01-03 15:51:47 +0900

    debug

--- DCTnet.py
+++ DCTnet.py
@@ -94,7 +94,7 @@ class DCTnet2(nn.Module):
         super(DCTnet2, self).__init__()
 
         self.conv01 = nn.Conv2d(3, 192, 8, stride = 8, bias = False)
-        W = kernelDCT2d2()
+        W = kernelDCT2d2()[:, 0, np.newaxis, :, :]
         self.conv01.weight = nn.Parameter(torch.Tensor(W), requires_grad=False)
         # (28, 28, 192)
 
